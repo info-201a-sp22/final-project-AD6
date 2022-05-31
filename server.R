9346c83efec2f128c7ad37c8164469a03c54e093
evelling@uw.edu <your-email-address>
2022-05-30 17:00:44 -0700

    added the first page of data

--- server.R
+++ server.R
@@ -1,5 +1,50 @@
 library(ggplot2)
 library(plotly)
 library(dplyr)
+nyc_data <- read.csv("scores.csv", stringsAsFactors = FALSE)
+
 
 server <- function(input, output) 
+# Introduction Page 
+
+# First Page (scatterplot)
+  output$scatter_plot <- renderPlotly({
+    plot_data <- nyc_data %>%
+      filter(Borough %in% input$borough_pick,
+             Student.Enrollment <= input$student_enrollment) 
+    
+    plot2 <- plot_ly(plot_data,
+                     x = ~Average.Score..SAT.Math.,
+                     y = ~Average.Score..SAT.Reading.,
+                     type = "scatter",
+                     mode = "markers",
+                     color = ~model,
+                     marker = list(size = 20),
+                     # interactive texts
+                     text = ~paste("<b>Neighborhood:</b>",
+                                   plot_data$Borough,
+                                   "<br>",
+                                   "<b>School Name:</b>",
+                                   plot_data$School.Name,
+                                   "<br>",
+                                   "<b>Average SAT Math Score:</b>",
+                                   plot_data$Average.Score..SAT.Math.,
+                                   "<br>",
+                                   "<b>Average SAT Reading Score:</b>",
+                                   plot_data$verage.Score..SAT.Reading.),
+                     hoverinfo = "text") %>%
+      
+      # set title, x, and y axis labels
+      layout(title = "Average SAT Scores by Neighborhood",
+             xaxis = list(title = "Average SAT Math Score"),
+             yaxis = list(title = "Average SAT Reading Score"))
+    
+    # return the scatter plot
+    return(plot2)
+    
+    
+    
+    
+    
+    
+  }) 
--- ui.R
+++ ui.R
@@ -6,20 +6,57 @@ library(shiny)
 library(stringr)
 library(bslib)
 library(markdown)
+nyc_data <- read.csv("scores.csv", stringsAsFactors = FALSE)
+
 
 
 
 intro_tab <- tabPanel(
   "Introduction",
-  
   fluidPage(
     h1("Analysis of NYC SAT Scores", align = "center"),
     h3("Research Questions", align = "left"),
     h3("Data"),
-    h3("Limitations")))
+    h3("Limitations"))) 
+
+# Data needed for first page widgets #
+# city miles range
+student.enrollment <- range(nyc_data$Student.Enrollment)
+# manufacturer types
+neighborhood_pick <- unique(nyc_data$Borough)
+
+# First Page #
+# selectInput drop down menu to select neighborhood
+student.enrollment <- selectInput(inputId = "borough_pick",
+                                  label = "Select Neighborhood",
+                                  choices = neighborhood_pick,
+                                  selected = neighborhood_pick[1],
+                                  multiple = FALSE)
+
+# sliderInput for setting only the maximum number of students 
+city_slider <- sliderInput(inputId = "student_enrollment",
+                           label = "Number of Students Enrolled",
+                           min = student.enrollment[1],
+                           max = student.enrollment[2],
+                           value = student.enrollment[2],
+                           step = 1)
+
+# Page setup #
+first_page <- tabsetPanel(
+  tabPanel("SAT Scores and Student Enrollment",
+           fluidRow(
+             column(borough_pick, width = 6),
+             column(student_enrollment, width = 6)
+           ),
+           fluidRow(
+             column(plotlyOutput("scatter_plot"), width = 12))
+  )
+)
+
 
 
 ui <- navbarPage(
   "NYC SAT Scores",
-  intro_tab
+  intro_tab, 
+  first_page
 )
